846b374c4a76da3f0cee403cbc2d01f3ebb57f28
IreneCreadora <nargiza.dancer@gmail.com>
2022-11-15 19:53:38 +0200

    fixed for of

--- src/js/saveDataAtLocalStorage.js
+++ src/js/saveDataAtLocalStorage.js
@@ -1,5 +1,5 @@
 export function saveDataAtLocalStorage(items) {
-  for (item of items) {
+  for (let item of items) {
     const jobData = JSON.stringify(item);
     const key = JSON.stringify(item.id);
     localStorage.setItem(key, jobData);
